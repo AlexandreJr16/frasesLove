18b27e7c5080bab90f348289fc39334592d83575
ALEXANDRE PEREIRA DE SOUZA JUNIOR <227067@fmm.local>
2023-10-05 13:49:59 -0400

    all

---
+++ app/src/main/java/com/example/geradorfrases/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.geradorfrases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void GerarFrase(View view){
+        String[] frases = {"Amor é fogo que arde sem doer", "i love u tomate"};
+        int num = new Random().nextInt(frases.length);
+        
+    }
+}
